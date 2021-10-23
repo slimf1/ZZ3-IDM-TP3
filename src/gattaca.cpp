f59aadb911ec899ca7716797611445c056e7e470
Barthelemy JUGLAIR <barthelemy.juglair@etu.uca.fr>
2021-10-23 12:23:34 +0200

    Add alphabet part for Q6.b

--- src/gattaca.cpp
+++ src/gattaca.cpp
@@ -17,7 +17,6 @@ char peptideGen(CLHEP::HepRandomEngine & engine){
     return chars[i];
 }
 
-
 uint64_t Func1(const std::string & target, CLHEP::HepRandomEngine & engine){
     std::stringstream ss;
     uint64_t count = 0;
@@ -36,17 +35,59 @@ uint64_t Func1(const std::string & target, CLHEP::HepRandomEngine & engine){
 }
 
 
+char asciiGen(CLHEP::HepRandomEngine & engine){
+    CLHEP::RandFlat aFlat(engine);
+    double x = aFlat.fire();
+    // [0,1] - > [65,122]
+    char c = static_cast<int>(x * 57 + 65);
+    // std::cout << x << " " << x * 61 + 65 << "\n";
+    // std::cout << static_cast<int>(x * 61 + 65) << " "<< c << "\n"
+    return c;
+}
+
+
+
+
+
+uint64_t Func2(const std::string & target, CLHEP::HepRandomEngine & engine, uint16_t targetSize){
+    std::stringstream ss;
+    uint64_t count = 0;
+    do{
+        ss.str(""); 
+        for(uint64_t i=0; i<targetSize; i++){
+            ss << asciiGen(engine);
+        }
+        count++;
+        if(count%10000==0){
+            std::cout << "\t" << count/10000 << "\n";
+        }
+    }while(ss.str() != target);
+    std::cout << "\n\n\n" << ss.str() << "\n";
+    return count;
+}
+
+
 
 int main() {
     CLHEP::MTwistEngine mtEngine;
     uint64_t N = 5e3+10;
-    uint64_t summ = 0;
-    uint64_t nb;
-    for(uint64_t i=0; i<N; i++){
-        nb = Func1("GATTACA", mtEngine);
-        // std::cout << nb << "\n";
-        summ+=nb;
+    // uint64_t summ = 0;
+    // uint64_t nb;
+
+    // Gen of GATTACA
+    // for(uint64_t i=0; i<N; i++){
+    //     nb = Func1("GATTACA", mtEngine, 7);
+    //     // std::cout << nb << "\n";
+    //     summ+=nb;
+    // }
+    // std::cout << static_cast<double>(summ)/N << "\n";
+
+    // Gen of ascii char
+    std::string s = "Le hasard n’écrit pas de programmes ou de gènes";
+    for(uint64_t i=0; i<1; i++){
+        Func2(s, mtEngine, 47);
     }
-    std::cout << static_cast<double>(summ)/N << "\n";
+
+    
 
 }
